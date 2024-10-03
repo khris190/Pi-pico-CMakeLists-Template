7f5198409ce2c7246bb56383e23484475816f47b
khris190 <khris10@interia.pl>
2024-10-03 16:36:52 +0200

    feat: change default code to pico w blink

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
-#include <hardware/clocks.h>
+#include "pico/cyw43_arch.h"
 #include <pico/stdio.h>
-#define DELAY 500
-int main() { set_sys_clock_khz(18000, true); }
+
+int main()
+{
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
